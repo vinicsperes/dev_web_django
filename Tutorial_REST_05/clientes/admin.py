eea7f0a0efead2ed504c7cb17ebece3704db33f5
ohperes <peresvini2@gmail.com>
2022-10-06 09:35:13 -0300

    Add some filters for clients

--- Tutorial_REST_05/clientes/admin.py
+++ Tutorial_REST_05/clientes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Cliente
+from clientes.models import Cliente
+class Clientes(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'cpf', 'rg', 'celular', 'ativo')
+  list_display_links = ('id', 'nome')
+  search_fields = ('nome',)
+  list_filter = ('ativo',)
+  list_editable = ('ativo',)
+  list_per_page = 10
+  ordering = ('nome',)
 
-admin.site.register(Cliente)
+admin.site.register(Cliente, Clientes)
--- Tutorial_REST_05/config/settings.py
+++ Tutorial_REST_05/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'clientes',
-    'rest_framework'
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+'PAGE_SIZE': 10
+}
